62baabcb9e91a1a09bfc5e8e9439f8954ff71fe4
great-create <sam9635521@gmail.com>
2025-09-26 05:58:44 +0800

    Update demo32.c

--- demo32.c
+++ demo32.c
@@ -1,65 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "LCD.h"
-#include "Keypad.h"
-#include "LED.h"
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
 
-char password[4];
-char input[4];
-int input_index = 0;
-bool password_generated = false;
 
-void generate_password() {
-    for (int i = 0; i < 4; i++) {
-        password[i] = '1' + rand() % 6; // 產生 '1'~'6'
-        display_seven_segment(i, password[i]); // 顯示在七段
-    }
-    password_generated = true;
-}
+void showled(void){
+    for(i=0;i<2;i++){
+			PC12 =0; PC13 =0; PC14 =0; PC15 =0; // turn off LED
+			CLK_SysTickDelay(300000);	 // 0.3Delay 	
+		
+			PC12 =1; PC13 =1;PC14 =1; PC15 =1; // turn on LED
+			CLK_SysTickDelay(300000);	 // 0.3Delay 
+		}
 
-void clear_input() {
-    input_index = 0;
-    clear_LCD();
-}
+]
 
-void check_password() {
-    if (input_index == 0) {
-        printS(0, 0, "NULL");
-        return;
+
+int main(void)
+{
+	int i;
+    SYS_Init(); 
+    GPIO_SetMode(PC, (BIT12|BIT13|BIT14|BIT15), GPIO_MODE_OUTPUT);
+	
+    while(1) {
+        
     }
 
-    if (memcmp(password, input, 4) == 0) {
-        printS(0, 0, "PASS");
-        led_marquee();
-    } else {
-        printS(0, 0, "ERROR");
-        led_flash_all();
-    }
 }
 
-void keypad_handler(char key) {
-    if (key >= '1' && key <= '6') {
-        if (input_index < 4) {
-            input[input_index++] = key;
-        }
-    } else if (key == 'R') {
-        generate_password();
-    } else if (key == 'C') {
-        clear_input();
-    } else if (key == 'O') {
-        check_password();
-    }
-}
 
-int main() {
-    init_LCD();
-    init_keypad();
-    init_LED();
-    srand(TIMER2->TDR); // 使用 Timer 作為亂數種子
 
-    while (1) {
-        char key = get_keypad_input();
-        keypad_handler(key);
+
+
+//====================
+//LED
+/*
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+
+
+void showled(void){
+    for(i=0;i<2;i++){
+			PC12 =0; PC13 =0; PC14 =0; PC15 =0; // turn off LED
+			CLK_SysTickDelay(300000);	 // 0.3Delay 	
+		
+			PC12 =1; PC13 =1;PC14 =1; PC15 =1; // turn on LED
+			CLK_SysTickDelay(300000);	 // 0.3Delay 
+		}
+
+]
+
+
+int main(void)
+{
+	int i;
+    SYS_Init(); 
+    GPIO_SetMode(PC, (BIT12|BIT13|BIT14|BIT15), GPIO_MODE_OUTPUT);
+	
+    while(1) {
+        
     }
+
 }
+*/
